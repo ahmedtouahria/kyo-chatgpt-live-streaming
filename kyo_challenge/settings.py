e1e24faeb744ec3ad2a13ccb1c8c531881922183
“ahmedtouahria” <ahmedtouahria2001@gmail.com>
2023-03-15 21:16:52 +0100

    allowed host

--- kyo_challenge/settings.py
+++ kyo_challenge/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-i-h-g0@rpn^%w%t#!mgy9hj)p9qq-3otz&sovpewx)vbx_s!oo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 AUTH_USER_MODEL = 'account.User'
 
