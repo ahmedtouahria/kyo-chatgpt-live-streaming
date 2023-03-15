9e8daad5363a86b256e5b1f0065184bfd3adfbbd
“ahmedtouahria” <ahmedtouahria2001@gmail.com>
2023-03-15 21:08:04 +0100

    add requirements.txt

--- chatgpt/views.py
+++ chatgpt/views.py
@@ -3,13 +3,14 @@ from .utils import chatGPT_interact
 from rest_framework.response import Response       
 import openai
 
-openai.api_key = "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"
+openai.api_key = "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"
 
 
 class ChatGptResponseView(APIView):
     def post(self,request,*args):
         text = request.data.get("text")
-        response = openai.Completion.create(
+        try:
+            response = openai.Completion.create(
             model="text-davinci-003",
             prompt=text,
             temperature=0.7,
@@ -18,4 +19,7 @@ class ChatGptResponseView(APIView):
             frequency_penalty=0,
             presence_penalty=0
             )       
+        except Exception as e:
+            return Response({"success":False,"response":str(e)})
+            
         return Response({"success":True,"response":response},status=200)
